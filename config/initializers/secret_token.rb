cd61ca04e90bfa8097e025a8937ff0fc8604b8c5
root <root@rails.(none)>
2012-03-16 17:10:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pghconnects::Application.config.secret_token = 'db03c2193fc9e8f27a62c73d99fa690f42810c2c124977f5cb9150c7a11cdbb5df4ad88710f2107a9201981a1aa10b917f7c564fbf34a1f00fa87f3fd8d31874'
